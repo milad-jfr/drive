6b847d557d5e38d0ee8c00a64bbe0977b2e738d7
Milad <j.milad@gmail.com>
2026-05-07 03:44:03 +0330

    Update worker.js

--- bridge/worker.js
+++ bridge/worker.js
@@ -1,14 +1,10 @@
-const fs = require("fs");
 const { chromium } = require("playwright");
+const fs = require("fs");
 
 (async () => {
-  const url = process.argv[2];
-  const outfile = process.argv[3];
-
-  if (!url || !outfile) {
-    console.error("Missing args");
-    process.exit(1);
-  }
+  const job = JSON.parse(fs.readFileSync(process.argv[2], "utf8"));
+  const id = job.id;
+  const url = job.url;
 
   const browser = await chromium.launch();
   const page = await browser.newPage();
@@ -16,7 +12,10 @@ const { chromium } = require("playwright");
   await page.goto(url, { waitUntil: "networkidle" });
 
   const html = await page.content();
-  fs.writeFileSync(outfile, html);
+
+  fs.writeFileSync(`bridge/results/${id}.html`, html);
 
   await browser.close();
+
+  console.log("done:", id);
 })();
